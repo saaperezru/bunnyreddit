852cfaf9d6c32ac5e8e8d8e9db325f22fbb58254
saaperezru <saaperezru@gmail.com>
2015-01-10 00:42:26 -0500

    Deleting swap vim files

--- README.md
+++ README.md
@@ -1,3 +1,9 @@
-virtualenv env
-source env/bin/activate
+== Deploy instructions ==
+
+Make sure you have both Python and virtualenv installed and then execute:
+
+virtualenvenv ./
+source ./bin/activate
 pip install -r req.txt
+cd bunnyreddit/
+python 
--- bunnyreddit/bunnyreddit/helpers.py
+++ bunnyreddit/bunnyreddit/helpers.py
@@ -9,9 +9,10 @@ api_key = "e7675939164ae130c817ab067d4ca7dd"
 
 class RedditPost:
 
-    def __init__(self,title,url):
+    def __init__(self,title,url,name):
         self.title = title
         self.url = url
+        self.name = name
 
 def sendProj(title,script,test=1):
     req = requests.post(bunny_url+'/projects/addSpeedy',
@@ -31,6 +32,15 @@ def getProj(bid):
     data = simplejson.loads(req.text)
     return data['reads'][0]['urls']['part001']['original']
 
+def getPost(name):
+    req = requests.post(reddit_url+'/by_id/'+name+'.json',
+       verify=False)
+    req.headers['User-Agent'] =  'bunnyreddit/0.1'
+    if(len(simplejson.loads(req.text)['data']['children'])<1):
+        raise Exception("No post found by name" + name)
+    data = simplejson.loads(req.text)['data']['children'][0]['data']
+    return RedditPost(data['title'],data['url'],data['name'])
+
 def getTrending(number):
     req = requests.post(reddit_url+'/r/hot.json',
        data={
@@ -44,5 +54,5 @@ def getTrending(number):
     ret = []
     for i in data['data']['children']:
         post = i['data']
-        ret.append(RedditPost(post['title'],post['url']))
+        ret.append(RedditPost(post['title'],post['url'],post['name']))
     return ret
